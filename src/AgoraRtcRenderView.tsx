fb80d4e972d9fba6258479fdc45c1fc9d28ff83e
HUI <luxuhui@agora.io>
2022-08-24 16:43:34 +0800

    chore: optimize

--- src/AgoraRtcRenderView.tsx
+++ src/AgoraRtcRenderView.tsx
@@ -1,6 +1,8 @@
 import { HostComponent, requireNativeComponent } from 'react-native';
 
 import IAgoraRtcRenderView from './internal/IAgoraRtcRenderView';
+import { VideoCanvas } from './AgoraBase';
+import { RtcConnection } from './IAgoraRtcEngineEx';
 
 const AgoraRtcSurfaceView = requireNativeComponent<{ callApi: object }>(
   'AgoraRtcSurfaceView'
@@ -10,9 +12,24 @@ const AgoraRtcTextureView = requireNativeComponent<{ callApi: object }>(
 );
 
 /**
+ * @ignore
+ */
+export interface RtcRendererViewProps {
+  /**
+   * @ignore
+   */
+  canvas: VideoCanvas;
+
+  /**
+   * @ignore
+   */
+  connection?: RtcConnection;
+}
+
+/**
  * Properties of the RtcSurfaceView.
  */
-export interface RtcSurfaceViewProps {
+export interface RtcSurfaceViewProps extends RtcRendererViewProps {
   /**
    * Controls whether to place the surface of the RtcSurfaceView on top of the window: true: Place it on top of the window.
    * false: Do not place it on top of another RtcSurfaceView in the window.
@@ -32,7 +49,7 @@ export class RtcSurfaceView extends IAgoraRtcRenderView<RtcSurfaceViewProps> {
   }
 }
 
-export class RtcTextureView extends IAgoraRtcRenderView<object> {
+export class RtcTextureView extends IAgoraRtcRenderView<RtcRendererViewProps> {
   get view(): HostComponent<{ callApi: object }> {
     return AgoraRtcTextureView;
   }
